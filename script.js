4cff0ddd423348aaccd038839fb74c3ed8f3c65c
Lillangg <123380111+Lillangg@users.noreply.github.com>
2025-06-26 15:27:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 let viz;
 
 const vizUrls = {
-  StrukturData1: "https://public.tableau.com/views/MSyamsulRizalAmri2SIMBStrukturData/Dashboard1",
-  StrukturData2: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri_2SIMB_2431011MAP_GEOSPASIAL/Dashboard6",
-  SampleSuperStore: "https://public.tableau.com/views/DashboardSampleSuperStore_17507763080410/Dashboard1?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
-  BarChart: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011_Project10/Dashboard1",
-  LineChart: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project_10_Line_Chart/Dashboard1",
-  PieChart: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011_Project10_Pie_Chart/Dashboard1",
-  MapChart: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project_10_Map_Chart/Dashboard1",
-  ScatterPlot: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Scatter_Plot/Dashboard1",
-  BubbleChart: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Bubble_Chart/Dashboard1",
-  TreeMap: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Tree_Map/Dashboard1",
-  GanttChart: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Gantt_Chart/Dashboard1",
-  LollipopChart: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Lolipop_Chart/Dashboard1",
-  WordCloud: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Word_Cloud/Dashboard1",
-  BoxPilot: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Box_Pilot/Dashboard1",
+ 
+  SampleSuperStore: "https://public.tableau.com/views/dashboardd_17509251665780/Dashboard1?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
+  BarChart: "https://public.tableau.com/views/PenjualanTerendahdanTertinggi/Dashboard4?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
+  LineChart: "https://public.tableau.com/views/TotalPenjualanPertahun/Dashboard3?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
+  PieChart: "https://public.tableau.com/views/DonatChart_17460218932840/Dashboard5?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
+  MapChart: "https://public.tableau.com/views/MAPGEOSPASIAL/Dashboard1?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
+  ScatterPlot: "https://public.tableau.com/views/SCATTERPLOT_17417879254070/Dashboard2?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
+  BubbleChart: "https://public.tableau.com/views/BUBBLECHART_17417882765170/Dashboard4?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
+  TreeMap: "https://public.tableau.com/views/TREEMAPS_17417882317090/Dashboard3?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
+  GanttChart: "https://public.tableau.com/views/GanttChart_17417905819630/Dashboard6?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
+  LollipopChart: "https://public.tableau.com/views/lolipopchart_17417902410000/Dashboard5?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
+  WordCloud: "https://public.tableau.com/views/world_17509260386670/Dashboard1?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
+  BoxPilot: "https://public.tableau.com/views/forbox/Dashboard5?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
   Histogram: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Histogram_Chart/Dashboard1?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
 };
 
